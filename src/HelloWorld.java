2667057b2211515d7e0d5ccf5455701273ee85fc
tourneureric <tourneur.eric@gmail.com>
2019-05-16 12:01:35 +0200

    Git add branch test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Eclipse GitHub Integration using token key");
+		System.out.println("Git add branch test");
+
 	}
 
 }
