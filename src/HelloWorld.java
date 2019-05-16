a4837da6ed27fedd4072d007765b213e78f37040
tourneureric <tourneur.eric@gmail.com>
2019-05-16 15:59:27 +0200

    f***

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Eclipse GitHub Integration using token key");
+		System.out.println("Git modify branch master");
+		System.out.println("Git add branch test");
+		System.out.println("Git modify branch test");
+		System.out.println("Git merge test to master");
+		System.out.println("Git merge conflict soved ok");
+		System.out.println("Git removed .class file ok");
+	}
+
+}
