fdfb67a9e8e0d5e7a0a266b46a8a5de79609e78e
tourneureric <tourneur.eric@gmail.com>
2019-05-16 12:09:36 +0200

    Git modify branch master

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Eclipse GitHub Integration using token key");
+		System.out.println("Git modify branch master");
 	}
 
 }
