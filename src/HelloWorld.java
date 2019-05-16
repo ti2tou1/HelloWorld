2ecd5fb223b93cf470c724b8138a610dea939bc8
tourneureric <tourneur.eric@gmail.com>
2019-05-16 15:30:19 +0200

    Git merge conflict soved ok

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld {
 		System.out.println("Git add branch test");
 		System.out.println("Git modify branch test");
 		System.out.println("Git merge test to master");
+		System.out.println("Git merge conflict soved ok");
 	}
 
 }
